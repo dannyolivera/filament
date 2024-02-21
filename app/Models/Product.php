8ef361dba32771ef707bf169a9557b67f9501d34
Danny Olivera <dannyoliverap2713@gmail.com>
2024-02-20 21:29:54 -0500

    :rocket: New app filament

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['description', 'price', 'stock', 'categories_id'];
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+}
